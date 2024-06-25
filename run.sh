3ade7466ab1dc6a5d10dee185f929e136513acc8
rishi kalra <=>
2024-06-25 23:22:45 +0530

    eh

---
+++ run.sh
@@ -0,0 +1,7 @@
+echo "# SOC-graphs-empire" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/rISHI-kALRA/SOC-graphs-empire.git
+git push -u origin main
